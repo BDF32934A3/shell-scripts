2519a0560df355fd4af3fb1f62a08b1e93b0b6fa
BDF32934A3 <46801776+BDF32934A3@users.noreply.github.com>
2019-06-14 20:36:12 -0500

    Added unattended update

--- installPTF.sh
+++ installPTF.sh
@@ -2,5 +2,16 @@
 apt-get update -y # Updates repo
 apt-get install python git -y # Installs python and git
 git clone https://github.com/trustedsec/ptf /opt/ptf # Clones ptf to /opt/ptf
-cd /opt/ptf && ./ptf # Executes ptf
- 
+# Executes ptf and runs unattended updates
+cd /opt/ptf && ./ptf <<EOF
+use modules/exploitation/install_update_all
+yes
+use modules/intelligence-gathering/install_update_all
+yes
+use modules/post-exploitation/install_update_all
+yes
+use modules/powershell/install_update_all
+yes
+use modules/vulnerability-analysis/install_update_all
+cd /pentest
+EOF
