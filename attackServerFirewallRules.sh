9af3f1153417c494e8ed1dc2fe97821731aacffd
BDF32934A3 <46801776+BDF32934A3@users.noreply.github.com>
2019-06-15 00:30:40 -0500

    Update attackServerFirewallRules.sh

--- attackServerFirewallRules.sh
+++ attackServerFirewallRules.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 # Checks for the correct number of arguments
 if [ $# -ne 1 ]; then
@@ -8,13 +7,13 @@ else
   # Checks wheter ufw is installed and if it isn't it installs it
   if [ $(dpkg-query -W -f='${Status}' ufw 2>/dev/null | grep -c "ok installed") -eq 0 ];
     then
-      apt-get update
-      apt-get install ufw -y;
+      echo 'UFW is not installed.  Installing it now...'
+      apt-get update 1>/dev/null
+      apt-get install ufw -y 1>/dev/null;
   fi
 # Adds a rule that allows the specified ssh_src_network_or_host to connect to the ssh port and enables ufw
+echo "Adding rule allowing SSH connections from $1"
 ufw allow from $1 to any port 22
 ufw enable
 ufw status
 fi
-
-
