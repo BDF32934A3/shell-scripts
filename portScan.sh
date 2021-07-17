aace44e6ebf7a35131905d4d9615dc53f2fd130d
t3chn0t3s <46801776+t3chn0t3s@users.noreply.github.com>
2021-07-16 20:12:05 -0500

    Update portScan.sh
    Sent Stderr to /dev/null for nmap scans and added some [+] and [-]  to make some echos easier to read

--- portScan.sh
+++ portScan.sh
@@ -9,25 +9,25 @@ targetName=$1
 targetIP=$2
 
 function display_usage() {
-    echo "usage: $0 <targetName> <targetIP>"
+    echo "[-] usage: $0 <targetName> <targetIP>"
 }
 
 function create_folders() { # TODO Add error handling
     mkdir $targetName\_$targetIP  
     cd $targetName\_$targetIP && mkdir Port_Scan_Results Enumeration Exploits PrivEsc Loot Tools
-    echo "Created folders for organization"
+    echo "[+] Created folders for organization"
 }
 
 function run_port_scan() {
-    echo "Starting port scan please wait..."
-    ports=$(nmap -Pn -p- --min-rate=1000 -T4 $targetIP --open | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
-    echo "Finished running Port Scan.  The open ports are: $ports"
+    echo "[+] Starting port scan please wait..."
+    ports=$(nmap -Pn -p- --min-rate=1000 -T4 $targetIP --open 2>/dev/null | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
+    echo "[+] Finished running Port Scan.  The open ports are: $ports"
 }
 
 function enum_open_ports() {
-    echo "Starting enumeration of open ports please wait..."
-    nmap -Pn -A $targetIP -p$ports -oA Port_Scan_Results/$targetName\_$targetIP
-    echo "Finished enumeration of open ports.  Results stored in the Port_Scan_Results folder"
+    echo "[+] Starting enumeration of open ports please wait..."
+    nmap -Pn -A $targetIP -p$ports -oA Port_Scan_Results/$targetName\_$targetIP 2>/dev/null 
+    echo "[+] Finished enumeration of open ports.  Results stored in the Port_Scan_Results folder"
 }
 
 # Checks whether two arguments are provided
